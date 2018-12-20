2025e440ff4d26d1c920c04b4e2d000b20484cfb
Peter Dulacka <dulacka@gmail.com>
2018-12-20 15:15:23 +0100

    Updating font awesome to the latest version, upgrading deprecated icons

--- src/DashboardModule.php
+++ src/DashboardModule.php
@@ -13,9 +13,9 @@ class DashboardModule extends CrmModule
         // submenu items registered before this main menu item is attached should be ignored as it's only this module
         // responsibility to register the main menu item with proper label, icon and position
 
-        $mainMenu = new MenuItem('', '#dashboard', 'fa fa-dashboard', 5);
+        $mainMenu = new MenuItem('', '#dashboard', 'fa fa-tachometer-alt', 5);
 
-        $menuItem = new MenuItem('Dashboard', ':Dashboard:Dashboard:', 'fa fa-dashboard', 100);
+        $menuItem = new MenuItem('Dashboard', ':Dashboard:Dashboard:', 'fa fa-tachometer-alt', 100);
         $mainMenu->addChild($menuItem);
 
         $menuContainer->attachMenuItem($mainMenu);
