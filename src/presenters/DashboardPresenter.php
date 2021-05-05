e8793f6e76a7cb7e10058cb094441c4399ba68f5
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-05-05 10:23:15 +0200

    Add level (read / write) to access resources
    Every access resource can be either read or write access level. These
    levels should indicate if resource is used only for reading data or also
    for adding and changing data.
    
    This can be used when granting permissions to admin groups. Eg.
    - analytic team needs only to read information about users,
    - support team needs to also update user's data to fix customer's issues.
    
    Access levels are:
    
    - set as PHP doc annotation `@admin-access-level read|write`,
    - stored to DB when `php bin/command.php user:generate_access` is run
      (this should be part of your deploy process).
    
    See README for details.
    
    remp/crm#1661

--- src/presenters/DashboardPresenter.php
+++ src/presenters/DashboardPresenter.php
@@ -6,6 +6,9 @@ use Crm\AdminModule\Presenters\AdminPresenter;
 
 class DashboardPresenter extends AdminPresenter
 {
+    /**
+     * @admin-access-level read
+     */
     public function renderDefault()
     {
     }
