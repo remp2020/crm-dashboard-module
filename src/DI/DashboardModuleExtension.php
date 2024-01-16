2eb40d93bcd79caccf3ac7a18b39311a4cd87739
Dominik Skerhak <dominik.skerhak@gmail.com>
2024-01-16 09:40:22 +0100

    Add use statements for classes where we used FQN
    remp/crm#2228

--- src/DI/DashboardModuleExtension.php
+++ src/DI/DashboardModuleExtension.php
@@ -3,6 +3,7 @@
 namespace Crm\DashboardModule\DI;
 
 use Contributte\Translation\DI\TranslationProviderInterface;
+use Nette\Application\IPresenterFactory;
 use Nette\DI\CompilerExtension;
 
 final class DashboardModuleExtension extends CompilerExtension implements TranslationProviderInterface
@@ -19,7 +20,7 @@ final class DashboardModuleExtension extends CompilerExtension implements Transl
     {
         $builder = $this->getContainerBuilder();
         // load presenters from extension to Nette
-        $builder->getDefinition($builder->getByType(\Nette\Application\IPresenterFactory::class))
+        $builder->getDefinition($builder->getByType(IPresenterFactory::class))
             ->addSetup('setMapping', [['Dashboard' => 'Crm\DashboardModule\Presenters\*Presenter']]);
     }
 
